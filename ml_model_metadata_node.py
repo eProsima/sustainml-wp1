b88b33dd761955ff1eca68b7a4195d001c6acdc8
Javier Gil Avilés <118965699+Javgilavi@users.noreply.github.com>
2025-03-26 14:16:08 +0100

    Integration for MVP (#6)
    * Refs #22755: Add new dfki graph, functions and goal skip in metadata
    
    Signed-off-by: Javier Gil Aviles <javiergil@eprosima.com>
    
    * Refs #22755: Add second new version of dkfi graph, funtions, and request for metrics. Add exceptions over errors.
    
    Signed-off-by: Javier Gil Aviles <javiergil@eprosima.com>
    
    * Refs #22755: Add new methods to rdfCode.py and options to request all MLModel data
    
    Signed-off-by: Javier Gil Aviles <javiergil@eprosima.com>
    
    * Refs #22755: Fix LLM not giving correct mlgoal
    
    Signed-off-by: Javier Gil Aviles <javiergil@eprosima.com>
    
    ---------
    
    Signed-off-by: Javier Gil Aviles <javiergil@eprosima.com>

--- ml_model_metadata_node.py
+++ ml_model_metadata_node.py
@@ -22,7 +22,11 @@ import threading
 import time
 import json
 
-from rdftool.rdfCode import get_mlgoals, get_inputs
+from rdftool.rdfCode import (
+    load_graph, get_problems, get_cover_tags, search_metrics_by_modalities, get_models_for_problem,
+    find_metrics_by_model, get_model_details, get_problems_for_cover_tag, get_all_metrics, get_modalities_input,
+    get_modalities_output
+)
 from ollama import Client
 
 # Whether to go on spinning or interrupt
@@ -43,7 +47,7 @@ def get_llm_response(client, model_version, problem_definition, prompt):
             {
                 'role': 'user',
                 'content': prompt,
-            },
+            }
         ])
         return response['message']['content']
     except Exception as e:
@@ -59,20 +63,55 @@ def task_callback(user_input, node_status, ml_model_metadata):
 
     print (f"Received Task: {user_input.task_id().problem_id()},{user_input.task_id().iteration_id()}")
 
+    try:
+        extra_data_bytes = user_input.extra_data()
+        extra_data_str = ''.join(chr(b) for b in extra_data_bytes)
+        extra_data_dict = json.loads(extra_data_str)
+
+        if "goal" in extra_data_dict and extra_data_dict["goal"] != "":
+            goal = extra_data_dict["goal"]
+            ml_model_metadata.ml_model_metadata().append(goal)
+            print(f"Skipped ML Model Metadata. ML Goal selected as input: {goal}")
+            return
+    except Exception as e:
+        print(f"No extra data was found: {e}")
+
     client = Client(host='http://localhost:11434')
-    graph_path = os.path.dirname(__file__)+'/CustomGraph.ttl'
+    graph = load_graph(os.path.dirname(__file__)+'/graph_v2.ttl')
 
     # Retrieve Possible Ml Goals from graph
-    mlgoals = get_mlgoals(graph_path)
+    try:
+        mlgoals = get_problems(graph)
+    except Exception as e:
+        print(f"Error in getting problems from MLModel graph: {e}")
+        return
     goals = ', '.join(mlgoals)
 
     # Select MLGoal Using Ollama llama 3
-    prompt = f"Which of the following machine learning Goals can be used to solve this problem (or part of it): {goals}. Answer with only  one of the Machine learning goals and with nothing else"
-    mlgoal = get_llm_response(client, "llama3", user_input.problem_definition(), prompt)
+    prompt = f"Which of the following machine learning Goals can be used to solve this problem (or part of it): {goals}. Answer with only one of the Machine learning goals and with nothing else"
 
-    if mlgoal != None and mlgoal in mlgoals:
+    if(user_input.problem_definition() == ""):
+        problem = user_input.problem_short_description()
+    else:
+        problem = user_input.problem_definition()
+
+    if(user_input.modality() != ""):
+        problem = f"{problem} with modality {user_input.modality()}"
+
+    print (f"Problem define: {problem}")   #Debugging
+    mlgoal = None
+    max_attempts = 3
+    attempt = 0
+    while attempt < max_attempts:
+        mlgoal = get_llm_response(client, "llama3", problem, prompt)
+        if mlgoal is not None and mlgoal in goals:
+            break
+        attempt += 1
+        print(f"Retry {attempt}: Response '{mlgoal}' is not among available goals. Retrying...")
+        
+    if mlgoal is not None and mlgoal in goals:
         ml_model_metadata.ml_model_metadata().append(mlgoal)
-        print (f"Selected ML Goal: {mlgoal}")
+        print(f"Selected ML Goal: {mlgoal}")
     else:
         raise Exception(f"Failed to determine ML goal for task {user_input.task_id()}.")
 
@@ -86,20 +125,181 @@ def configuration_callback(req, res):
     if req.configuration() == "modality":
         res.node_id(req.node_id())
         res.transaction_id(req.transaction_id())
+        try:
+            # Retrieve Possible Ml Goals from graph
+            graph = load_graph(os.path.dirname(__file__)+'/graph_v2.ttl')
+            inputs = get_cover_tags(graph)
+            sorted_modalities = ', '.join(sorted(inputs))
 
-        # Retrieve Possible Ml Goals from graph
-        graph_path = os.path.dirname(__file__)+'/CustomGraph.ttl'
-        inputs = get_inputs(graph_path)
-        ins = ', '.join(inputs)
+            if sorted_modalities == "":
+                res.success(False)
+                res.err_code(1) # 0: No error || 1: Error
+            else:
+                res.success(True)
+                res.err_code(0) # 0: No error || 1: Error
+            print(f"Available Modalities: {sorted_modalities}") #debug
 
-        if ins == "":
+            inputs2 = get_problems(graph)
+            sorted_goals = ', '.join(sorted(inputs2[:-1]))  # TODO: fix overflow bug sending goals response to request
+
+            if sorted_goals == "":
+                res.success(False)
+                res.err_code(1) # 0: No error || 1: Error
+            else:
+                res.success(True)
+                res.err_code(0) # 0: No error || 1: Error
+            print(f"Available Goals: {sorted_goals}")   #debug
+
+            json_str = json.dumps(dict(modalities=sorted_modalities, goals=sorted_goals))
+            print(len(json_str))    #debug
+
+            res.configuration(json.dumps(dict(modalities=sorted_modalities, goals=sorted_goals)))
+        except Exception as e:
+            print(f"Error getting goals and modalities from request: {e}")
+            res.success(False)
+            res.err_code(1) # 0: No error || 1: Error
+
+    elif "in_out_modalities" in req.configuration():
+        res.node_id(req.node_id())
+        res.transaction_id(req.transaction_id())
+        try:
+            # Retrieve Possible Ml Inputs and Outputs modalities
+            graph = load_graph(os.path.dirname(__file__)+'/graph_v2.ttl')
+            inputs = get_modalities_input(graph)
+            sorted_inputs = ', '.join(sorted(inputs))
+            outputs = get_modalities_output(graph)
+            sorted_outputs = ', '.join(sorted(outputs))
+
+            if sorted_inputs == "" or sorted_outputs == "":
+                res.success(False)
+                res.err_code(1) # 0: No error || 1: Error
+            else:
+                res.success(True)
+                res.err_code(0) # 0: No error || 1: Error
+            print(f"Available Input Modalities: {sorted_inputs}") #debug
+            print(f"Available Output Modalities: {sorted_outputs}") #debug
+
+            res.configuration(json.dumps(dict(inputs=sorted_inputs, outputs=sorted_outputs)))
+
+        except Exception as e:
+            print(f"Error getting inputs and outputs modalities from request: {e}")
+            res.success(False)
+            res.err_code(1) # 0: No error || 1: Error
+
+    elif "metrics" in req.configuration():
+        res.node_id(req.node_id())
+        res.transaction_id(req.transaction_id())
+
+        graph = load_graph(os.path.dirname(__file__)+'/graph_v2.ttl')
+
+        # Extracts datas for metrics reception
+        config_content = req.configuration()[len("metrics, "):]  # "metrics, <metric_req_type>: <req_type_values>"
+
+        try:
+            metric_req_type, req_type_values = config_content.split(":", 1)
+            metric_req_type = metric_req_type.strip()
+            req_type_values = req_type_values.strip()
+
+            if metric_req_type == "modality":
+                input_modality, output_modality = req_type_values.split(",", 1)
+                input_modality = input_modality.strip()
+                output_modality = output_modality.strip()
+                metrics = search_metrics_by_modalities(graph, input_modality, output_modality)
+                all_metrics = []
+                for problem, metrics_list in metrics.items():
+                    for model, m in metrics_list.items():
+                        if isinstance(m, list):
+                            for metric in m:
+                                if metric not in all_metrics:
+                                    all_metrics.append(metric)
+                        else:
+                            if m not in all_metrics:
+                                all_metrics.append(m)
+                sorted_metrics = ', '.join(sorted(all_metrics))
+
+            elif metric_req_type == "problem":
+                models = get_models_for_problem(graph, req_type_values)
+                all_metrics = []
+
+                for model,downloads in models:
+                    metrics = find_metrics_by_model(graph, model)
+                    if isinstance(metrics, list):
+                        all_metrics.extend(metrics)
+                    else:
+                        all_metrics.append(metrics)
+
+                sorted_metrics = ', '.join(sorted(all_metrics))
+
+            elif metric_req_type == "all":
+                metrics = get_all_metrics(graph)
+                sorted_metrics = ', '.join(sorted(metrics))
+
+            else:
+                res.success(False)
+                res.err_code(1) # 0: No error || 1: Error
+
+        except Exception as e:
+            print(f"Error getting metrics from request: {e}")
+            res.success(False)
+            res.err_code(1) # 0: No error || 1: Error
+
+        if sorted_metrics == "":
             res.success(False)
             res.err_code(1) # 0: No error || 1: Error
         else:
             res.success(True)
             res.err_code(0) # 0: No error || 1: Error
-        print (f"Available Modalities: {ins}")
-        res.configuration(json.dumps(dict(modalities=ins)))
+        print(f"Available Metrics: {sorted_metrics}")
+
+        res.configuration(json.dumps(dict(metrics=sorted_metrics)))
+
+    elif 'mode_info' in req.configuration():
+        res.node_id(req.node_id())
+        res.transaction_id(req.transaction_id())
+
+        graph = load_graph(os.path.dirname(__file__) + '/graph_v2.ttl')
+        try:
+            model = req.configuration()[len("mode_info, "):]
+            details = get_model_details(graph, model)
+
+            if not details:
+                res.success(False)
+                res.err_code(1)  # 0: No error || 1: Error
+            else:
+                res.success(True)
+                res.err_code(0)  # 0: No error || 1: Error
+
+            print(f"Model details for {model}: {details}")
+            res.configuration(json.dumps(details))
+        except Exception as e:
+            print(f"Error getting model details from request: {e}")
+            res.success(False)
+            res.err_code(1)
+
+    elif 'problem_from_modality' in req.configuration():
+        res.node_id(req.node_id())
+        res.transaction_id(req.transaction_id())
+
+        graph = load_graph(os.path.dirname(__file__) + '/graph_v2.ttl')
+        try:
+            modality = req.configuration()[len("problem_from_modality, "):]
+            goals = get_problems_for_cover_tag(graph, modality)
+            sorted_goals = ', '.join(sorted(goals))
+
+            if not sorted_goals:
+                res.success(False)
+                res.err_code(1)  # 0: No error || 1: Error
+            else:
+                res.success(True)
+                res.err_code(0)  # 0: No error || 1: Error
+
+            print(f"Problems for {modality}: {goals}")
+            res.configuration(json.dumps(dict(goals=sorted_goals)))
+
+        except Exception as e:
+            print(f"Error getting model details from request: {e}")
+            res.success(False)
+            res.err_code(1)
 
     else:
         # Dummy JSON configuration and implementation
